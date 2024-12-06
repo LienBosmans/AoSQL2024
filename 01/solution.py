dec0b22d2dba8ec72dba6470efbceec1be184f93
Lien <lienbosmans@live.com>
2024-12-06 22:28:51 +0100

    Updated files for day 1

--- 01/solution.py
+++ 01/solution.py
@@ -1,16 +1,17 @@
 import duckdb
 
-# Connect to DuckDB database
-con = duckdb.connect("quack01.db")
+## Connect to DuckDB database
+con = duckdb.connect("quack.db")
 
-# Set-up DuckDB database using the SQL dump provided
-file_path = 'sql_dump.txt'
+## Set-up DuckDB database using the SQL dump provided
+# file_path = 'example.txt' # One-time setup of the example data. You can delete `quack.db` if you want to run it again.
+file_path = 'setup.txt' # Setup of the actual data. It overwrites the example data.
 with open(file_path, 'r') as file:
     sql_dump = file.read()
 
 con.sql(sql_dump)
 
-#  Write a SQL query and save as solution.csv
+##  Write a SQL query and save as solution.csv
 con.sql(
 '''--sql
 with unnested_whishlist as (
